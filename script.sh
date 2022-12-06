0be0a7c510c6704e37870cb64c8d6270424ab819
root <root@ip-172-31-90-250.ec2.internal>
2022-12-06 21:15:27 +0000

    v2

--- script.sh
+++ script.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-
 # Copier le fichier de configuration sur le serveur
 
 cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
 
 cp ./config__files/ssh/Banner /etc/Banner
+
+echo "Nom user:"
+read username
+adduser $username
+usermod -a -G sudo $username
+
+echo"MDP"
+passwd $username
+
+echo "clé pub"
+read sshkey
+echo $sshkey >> ~/.ssh/authorized_keys
 
 
 # Redemarrer le service sshd 
